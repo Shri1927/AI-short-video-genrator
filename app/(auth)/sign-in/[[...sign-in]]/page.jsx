3da6544d26bd7dd546e4524cca3a68d9873f5351
Shrishail  Bidave <167215447+Shri1927@users.noreply.github.com>
2025-03-09 00:18:58 +0530

    fix

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -1,5 +1,14 @@
 import { SignIn } from '@clerk/nextjs'
+import Image from 'next/image'
 
 export default function Page() {
-  return <SignIn />
+  return <div className='grid grid-cols-1 md:grid-cols-2'>
+    <div>
+         <Image src={'/story.jpg'} alt="logo" height={500} width={500}
+         className='w-full object-contain' />
+    </div>
+    <div className='flex justify-center items-center h-screen'>
+          <SignIn />
+    </div>
+  </div>
 }
--- app/page.js
+++ app/page.js
@@ -1,7 +1,8 @@
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return <div>
-    <h1>hello</h1>
+    <h1 className="text-3xl font-bold">hello</h1>
+    <UserButton/>
   </div>
 }
