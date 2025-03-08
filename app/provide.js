c58532597bc46c9eaff5a479a65257103d21f914
Shrishail  Bidave <167215447+Shri1927@users.noreply.github.com>
2025-03-09 01:39:20 +0530

    profile added

--- app/provide.js
+++ app/provide.js
@@ -1,5 +1,32 @@
+'use client';
+import { Users } from "@/configs/schema";
+import { eq } from "drizzle-orm";
+import { useEffect } from "react";
+import { useUser } from "@clerk/nextjs";
+import {db} from "@/configs/db";
 
 function Provider({children}) {
+
+  const {user} = useUser();
+
+  useEffect( () => {
+     user&&isNewuser();
+  },[user]);
+
+  const isNewuser = async () => {
+    const result = await db.select().from(Users)
+    .where(eq(Users.email, user?.primaryEmailAddress?.primaryEmailAddress));
+     console.log(result);
+     
+    if(!result[0]){
+      await db.insert(Users).values({
+        name : user?.fullName,
+        email : user?.primaryEmailAddress?.primaryEmailAddress,
+        imgUrl : user?.imgUrl
+      });
+    }
+  }
+
   return (
     <div>
       {children}
